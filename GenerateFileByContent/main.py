51bfad9b7a27df5d5669dca0d70b604d50008d2e
volkansahin <bm.volkansahin@gmail.com>
2022-01-30 13:48:34 +0300

    file creation acc. to regex result

--- GenerateFileByContent/main.py
+++ GenerateFileByContent/main.py
@@ -2,31 +2,25 @@ import re
 import os
 
 
-def create_files(dir_name,file_name):
-    f = open(file_name, "r")
-    content = f.read()
-    f.close()
-
-    reg_str = "(?<=\\(#)[^\\)]*"
-    x = re.findall(reg_str, content)
+def create_files(dir_name, file_name, content):
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
-    for topic in x:
-        topic = topic.replace("-"," ").title().replace(" ","_", 1)
-        name = "./{0}/{1}.md".format(dir_name,topic)
-        if not os.path.exists(name) :
-            print("created:"+name)
-            f = open(name, "a")
-            f.close()
+    file_path = os.path.join(dir_name, file_name + ".md")
+    with open(file_path, "w+") as f:
+        f.write(content)
+
+
+def parse_topics(str_content):
+    reg_str = r"(?<=\#\s)(.+)([^#]*)"
+    x = re.findall(reg_str, str_content)
+    return x
 
 
 for d in os.listdir():
-    if d.endswith(".txt"):
-        dir_name,file_name = d.split("_")
-        create_files(dir_name,file_name)
 
-
-
-
-
+    if d.endswith(".md"):
+        content = open(d, "r").read()
+        list_title_content = parse_topics(content)
+        for title, content in list_title_content:
+            create_files(d.split('.')[0], title, content)
