85265e4bccd52229ce343feb99e36e496717fa2d
volkansahin <bm.volkansahin@gmail.com>
2022-01-31 20:14:44 +0300

    file creation acc. to regex result

--- GenerateFileByContent/main.py
+++ GenerateFileByContent/main.py
@@ -2,13 +2,13 @@ import re
 import os
 
 
-def create_files(dir_name, file_name, content):
+def create_files(dir_name, file_name, file_content):
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
-
+    file_name= "".join(x for x in file_name if x.isalnum() or x == ' ' or x == '.').strip()
     file_path = os.path.join(dir_name, file_name + ".md")
     with open(file_path, "w+") as f:
-        f.write(content)
+        f.write(file_content)
 
 
 def parse_topics(str_content):
@@ -18,7 +18,6 @@ def parse_topics(str_content):
 
 
 for d in os.listdir():
-
     if d.endswith(".md"):
         content = open(d, "r").read()
         list_title_content = parse_topics(content)
